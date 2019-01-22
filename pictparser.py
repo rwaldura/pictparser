9f2c5650868d2ca53a9b73cee2113339f040d0e9
Ren Waldura <rwaldura@users.noreply.github.com>
2019-01-22 15:24:56 -0800

    Fix to include last byte of resource
    If the resource ends with 2 bytes (rather than 4), they were not included. This resulted in a corrupted PICT files. 
    I also edited the regexps slightly, as they were not working for me. Python version difference?

--- pictparser.py
+++ pictparser.py
@@ -20,11 +20,10 @@ for l in out:
         continue
     # start of file
     if re.match('data \'PICT\'', l):
-        m = re.search('\(\d*\)', l)
-        num = m.group(0)
-        num = num.translate(None, '()')
+        m = re.search(' \((\d+)', l)
+        num = m.group(1)
         print "open "+num
-        working = open("res"+num+".pct", "wb")
+        working = open("pict"+num+".pct", "wb")
         for a in range(0, 512):
             s = struct.pack(">B", 0);
             working.write(s)
@@ -34,14 +33,15 @@ for l in out:
         print "done"
     # file data
     else:
-        cap = re.search('\".*\" ', l)
+        cap = re.search('\"([A-Z 0-9]+)\"', l)
         if cap is None:
             continue
-        data = cap.group(0)
-        data = data.translate(None, '"')
+        data = cap.group(1)
         for a in data.split(' '):
-            if len(a) != 4:
-                continue
-            first, second = a[:len(a)/2], a[len(a)/2:]
-            pdata = struct.pack(">BB", int(first, 16), int(second, 16))
-            working.write(pdata)
+            if len(a) == 4:
+                first, second = a[:len(a)/2], a[len(a)/2:]
+                pdata = struct.pack(">BB", int(first, 16), int(second, 16))
+                working.write(pdata)
+            if len(a) == 2:
+                pdata = struct.pack(">B", int(a, 16))
+                working.write(pdata)
